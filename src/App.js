1cd41178e761639954afa563b63127c3aeb1bceb
Gavin <Gavin.Gosling1@gmail.com>
2020-09-17 15:59:24 -0400

    fixed responsiveness

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@
 import React from 'react';
 import {FaGithub, FaLinkedin, FaStackOverflow, FaEnvelope, FaFilePdf, FaCode} from 'react-icons/fa';
 import Typist from 'react-typist';
-import {Divider, Message,Image} from 'semantic-ui-react';
 import { FadeIn, Bounce } from "animate-components";
-import 'semantic-ui-css/semantic.min.css';
 import ParticlesBg from 'particles-bg';
 import MouseParticles from "react-mouse-particles";
 import RingLoader from "react-spinners/RingLoader";
@@ -17,9 +15,13 @@ class App extends React.Component{
   constructor(props){
     super(props);
   }
+
   render(){
     let particles = true;
-    let h = document.height;
+    let spinner = false;
+    let showImage = true;
+    let h = (window.innerHeight > 0) ? window.innerHeight : window.screen.height;
+    let w = (window.innerWidth > 0) ? window.innerWidth : window.screen.width;
     var ua = navigator.userAgent.toLowerCase(); 
     if (ua.indexOf('safari') != -1) { 
       if (ua.indexOf('chrome') > -1) {
@@ -28,6 +30,16 @@ class App extends React.Component{
         particles = false;
       }
     }
+    console.log(w)
+    if(w <= 425){
+      particles = false;
+      //showImage = false;
+    }
+
+    if(h <= 780){
+      spinner = false;
+    }
+
     return (
       <div>
         {particles ? <ParticlesBg type="cobweb" num={100} color="123abc" bg={true} style={{height: h}}/> : null}
@@ -38,12 +50,10 @@ class App extends React.Component{
             <Typist cursor={{show: false, hiddenWhenDone: true}}>
                 <div style={{fontSize: "48px"}}>Gavin Gosling</div>
                 <Typist.Delay ms={500} />
-                <br/>
                 <div style={{fontSize: "30px"}}>Software Engineer</div>
             </Typist>
             </FadeIn>
           </div>
-          <Divider/>
           <Bounce>
           <FadeIn>
           <div>
@@ -65,12 +75,12 @@ class App extends React.Component{
           </div>
           </FadeIn>
           </Bounce>
-          <RingLoader
+          {spinner ? <RingLoader
             size={150}
             color={"#123abc"}
             loading={true}
             className="ring"
-          />
+          /> : null}
         </div>
         <div className="header-project">
           <div className="title">
@@ -81,18 +91,19 @@ class App extends React.Component{
               </FadeIn>
             </div>
           </div>
-        <Projects/>
+        <Projects showImage={showImage}/>
       </div>
     );
   }
 }
 
-function Projects(){
+function Projects(props){
+  let showImage = props.showImage;
   return (
     <div className="projects">
       {json.map((project, key) => 
         <div className="project">
-          <Message key={key}>
+          <div className="message" key={key}>
             <div className="flex-col">
               <div style={{fontSize:"32px", marginBottom: "1vh", textAlign:"left"}}>
                 {project.name}
@@ -107,13 +118,14 @@ function Projects(){
               </div>
             </div>
             <div className="flex-row upper-proj">
-              <Image src={project.image} size="small"/>
+              {showImage ? <img src={project.image} className="image"/> : null}
               <div className="flex-col lower-proj">
-                <div className="flex-text description">{project.description}</div>
-                <div className="flex-text libraries">{project.libraries}</div>
+                  <div className="flex-text description">{project.description}</div>
+                  <div className="flex-text libraries"><span style={{fontWeight: 'bold'}}>Language:</span>{project.language}</div>
+                  <div className="flex-text libraries"><span style={{fontWeight: 'bold'}}>Libraries:</span>{project.libraries}</div>
               </div>
             </div>
-          </Message>
+          </div>
         </div>
       )}
     </div>
